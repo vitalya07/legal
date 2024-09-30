3a0ff5abe0ac8c0c0f10203982f9423316f8414b
Виталий <saifutdinov_vitalii@mail.ru>
2024-09-30 16:53:19 +0500

    Исправил главную страницу и сделал страницу о компании, без адаптива

--- js/main.js
+++ js/main.js
@@ -1,85 +1,90 @@
-let selector = document.querySelector('#modal__phone');
-let im = new Inputmask("+7(999)999-99-99");
-im.mask(selector)
+if(window.location.pathname === 'index.html') {
+    let selector = document.querySelector('#modal__phone');
+    let im = new Inputmask("+7(999)999-99-99");
+    im.mask(selector);
 
 
-const validate = new window.JustValidate('#modal__form');
-validate.addField('#modal__name', [
-    {
-        rule: "required",
-        errorMessage: "*Введите имя"
-    },
-    {
-        rule: "minLength",
-        value: 2,
-        errorMessage: "*минимум 2 символа"
-    },
-]).addField('#modal__phone', [
-    {
-        validator: (value)=> {
-            const phones = selector.inputmask.unmaskedvalue()
-            return Boolean(Number(phones) && phones.length > 0)
+    const validate = new window.JustValidate('#modal__form');
+    validate.addField('#modal__name', [
+        {
+            rule: "required",
+            errorMessage: "*Введите имя"
         },
-        errorMessage: '*Введите телефон'
-    },
-    {
-        validator: (value)=> {
-            const phones = selector.inputmask.unmaskedvalue()
-            return Boolean(Number(phones) && phones.length === 10)
+        {
+            rule: "minLength",
+            value: 2,
+            errorMessage: "*минимум 2 символа"
         },
-        errorMessage: '*Введите телефон полностью'
-    },
-]).addField('#modal__email', [
-    {
-        rule: "required",
-        errorMessage: "*Введите email"
-    },
-    {
-        rule: "minLength",
-        value: 2,
-        errorMessage: "*минимум 2 символа"
-    },
-]).onSuccess(async function() {
-    let data = {
-        name: document.getElementById('name').value,
-        tel: document.getElementById('phone').value,
-        message: document.getElementById('msg').value
-    }
-    let responsive = await fetch("mail.php", {
-        method: "POST",
-        body:JSON.stringify(data),
-        headers: {
-            "Content-Type": "application/json; charset=UTF-8"
+    ]).addField('#modal__phone', [
+        {
+            validator: (value)=> {
+                const phones = selector.inputmask.unmaskedvalue()
+                return Boolean(Number(phones) && phones.length > 0)
+            },
+            errorMessage: '*Введите телефон'
+        },
+        {
+            validator: (value)=> {
+                const phones = selector.inputmask.unmaskedvalue()
+                return Boolean(Number(phones) && phones.length === 10)
+            },
+            errorMessage: '*Введите телефон полностью'
+        },
+    ]).addField('#modal__email', [
+        {
+            rule: "required",
+            errorMessage: "*Введите email"
+        },
+        {
+            rule: "minLength",
+            value: 2,
+            errorMessage: "*минимум 2 символа"
+        },
+    ]).onSuccess(async function() {
+        let data = {
+            name: document.getElementById('name').value,
+            tel: document.getElementById('phone').value,
+            message: document.getElementById('msg').value
         }
-    })
-    let result = await responsive.text();
-    alert(result)
-})
+        let responsive = await fetch("mail.php", {
+            method: "POST",
+            body:JSON.stringify(data),
+            headers: {
+                "Content-Type": "application/json; charset=UTF-8"
+            }
+        })
+        let result = await responsive.text();
+        alert(result)
+    });
 
+    document.querySelector('.information__btn').addEventListener('click', function() {
+        document.querySelector('.modal').classList.add('active');
+    });
+    document.querySelector('.clients__btn').addEventListener('click', function() {
+        document.querySelector('.modal').classList.add('active');
+    });
+    document.querySelector('.modal__close').addEventListener('click', function() {
+        document.querySelector('.modal').classList.remove('active');
+    });
+    document.querySelector('.hamburger').addEventListener('click', function() {
+        document.querySelector('.header').classList.toggle('open');
+    });
 
+    let tabsCitizens = document.getElementById('citizens-btn');
+    let tabsBisnes = document.getElementById('bisnes-btn');
 
-document.querySelector('.information__btn').addEventListener('click', function() {
-    document.querySelector('.modal').classList.add('active');
-});
-document.querySelector('.clients__btn').addEventListener('click', function() {
-    document.querySelector('.modal').classList.add('active');
-});
-document.querySelector('.modal__close').addEventListener('click', function() {
-    document.querySelector('.modal').classList.remove('active');
-});
-document.querySelector('.hamburger').addEventListener('click', function() {
-    document.querySelector('.header').classList.toggle('open');
-});
+    tabsCitizens.addEventListener('click', function() {
+        document.getElementById('bissnes').style.display = 'none'
+        document.getElementById('citizens').style.display = 'grid'
+    });
 
-let tabsCitizens = document.getElementById('citizens-btn');
-let tabsBisnes = document.getElementById('bisnes-btn');
-
-tabsCitizens.addEventListener('click', function() {
-    document.getElementById('bissnes').style.display = 'none'
-    document.getElementById('citizens').style.display = 'grid'
-});
-
-tabsBisnes.addEventListener('click', function() {
-    document.getElementById('citizens').style.display = 'none'
-    document.getElementById('bissnes').style.display = 'grid'
-})
+    tabsBisnes.addEventListener('click', function() {
+        document.getElementById('citizens').style.display = 'none'
+        document.getElementById('bissnes').style.display = 'grid'
+    });
+}
+if (window.location.pathname === '/about.html') {
+    document.querySelector('.hamburger').addEventListener('click', function() {
+        document.querySelector('.header').classList.toggle('open');
+    });    
+}
