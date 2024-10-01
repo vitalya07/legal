97cd7f2b360da0b93817f07b92fb01971654fc11
Виталий <saifutdinov_vitalii@mail.ru>
2024-10-02 02:32:28 +0500

    Сайт почти полностью готов

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-if(window.location.pathname === '/index.html') {
+function form() {
     let selector = document.querySelector('#modal__phone');
     let im = new Inputmask("+7(999)999-99-99");
     im.mask(selector);
@@ -56,20 +56,23 @@ if(window.location.pathname === '/index.html') {
         let result = await responsive.text();
         alert(result)
     });
+}
 
+function clickBtn() {
     document.querySelector('.information__btn').addEventListener('click', function() {
         document.querySelector('.modal').classList.add('active');
-    });
-    document.querySelector('.clients__btn').addEventListener('click', function() {
-        document.querySelector('.modal').classList.add('active');
-    });
+    });  
     document.querySelector('.modal__close').addEventListener('click', function() {
         document.querySelector('.modal').classList.remove('active');
     });
+   
+}
+function hamburger() {
     document.querySelector('.hamburger').addEventListener('click', function() {
         document.querySelector('.header').classList.toggle('open');
     });
-
+}
+function tabs() {
     let tabsCitizens = document.getElementById('citizens-btn');
     let tabsBisnes = document.getElementById('bisnes-btn');
 
@@ -82,48 +85,9 @@ if(window.location.pathname === '/index.html') {
         document.getElementById('citizens').style.display = 'none'
         document.getElementById('bissnes').style.display = 'grid'
     });
-};   
-
-
-
-
-if (window.location.pathname === '/about.html') {
-    document.querySelector('.hamburger').addEventListener('click', function() {
-        document.querySelector('.header').classList.toggle('open');
-    });    
 }
-
-if (window.location.pathname === '/services.html') {
-    document.querySelector('.hamburger').addEventListener('click', function() {
-        document.querySelector('.header').classList.toggle('open');
-    });
-    // let table = document.querySelectorAll('.services__table');
-    // let tableRow = document.querySelectorAll('.services__table-item--row');  
-    // let tableShow = document.querySelectorAll('.services__table-item--show');
-    // let btnShow = document.querySelectorAll('.services__table-show');
-    // tableRow.forEach((el, index) => {
-    //     if (index % 2 == 0) {
-    //         el.style.backgroundColor = '#f7f2c9';
-    //     }
-    // });    
-    
-    // btnShow.forEach(el => {
-    //     el.addEventListener('click', ()=> {
-    //         tableShow.forEach(el => {
-    //             el.classList.toggle('active')
-    //         })
-    //     })
-    // })
-
-    // btnShow.forEach((btn, index) => {
-    //     btn.addEventListener('click', () => {
-    //         const table = tableShow[index];
-    //         table.classList.toggle('active');
-    //     });
-    // });
-
+function tables() {
     let tables = document.querySelectorAll('.services__table');
-
     tables.forEach(table => {
         let tableRow = table.querySelectorAll('.services__table-item--row');  
         let tableShow = table.querySelectorAll('.services__table-item--show');
@@ -139,4 +103,40 @@ if (window.location.pathname === '/services.html') {
             });
         });
     });
+}
+// Скрипты для главной страницы
+if(window.location.pathname === '/index.html') {  
+    tabs();
+    clickBtn();
+    form();
+    hamburger();    
+};  
+// Скрипты для страницы "О компании"
+if (window.location.pathname === '/about.html') {
+    hamburger()   
+}
+// Скрипты для страницы "Цены и услуги"
+if (window.location.pathname === '/services.html') {
+    hamburger();
+    clickBtn();
+    form();    
+    document.querySelector('.questions__btn').addEventListener('click', function() {
+        document.querySelector('.modal').classList.add('active');
+    });
+    tables();
+}
+// Скрипты для Примеры дел
+if (window.location.pathname === '/examples.html') {
+    let tableItemConstruction = document.querySelectorAll('.construction__table-row');
+    tableItemConstruction.forEach((el, index) => {
+        if(index % 2 == 0) {
+            el.style.backgroundColor = '#c9c9c9'
+        }
+    });
+}
+// Скрипты для страницы "Контакты"
+if (window.location.pathname === '/contacts.html') {    
+    hamburger();      
+    clickBtn(); 
+    form();
 }
