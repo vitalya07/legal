2fce35093d1a74b3c079002cdab62427f43b76b7
Виталий <saifutdinov_vitalii@mail.ru>
2024-10-01 16:12:21 +0500

    Сделал таблицы с JS

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-if(window.location.pathname === 'index.html') {
+if(window.location.pathname === '/index.html') {
     let selector = document.querySelector('#modal__phone');
     let im = new Inputmask("+7(999)999-99-99");
     im.mask(selector);
@@ -82,9 +82,61 @@ if(window.location.pathname === 'index.html') {
         document.getElementById('citizens').style.display = 'none'
         document.getElementById('bissnes').style.display = 'grid'
     });
-}
+};   
+
+
+
+
 if (window.location.pathname === '/about.html') {
     document.querySelector('.hamburger').addEventListener('click', function() {
         document.querySelector('.header').classList.toggle('open');
     });    
+}
+
+if (window.location.pathname === '/services.html') {
+    document.querySelector('.hamburger').addEventListener('click', function() {
+        document.querySelector('.header').classList.toggle('open');
+    });
+    // let table = document.querySelectorAll('.services__table');
+    // let tableRow = document.querySelectorAll('.services__table-item--row');  
+    // let tableShow = document.querySelectorAll('.services__table-item--show');
+    // let btnShow = document.querySelectorAll('.services__table-show');
+    // tableRow.forEach((el, index) => {
+    //     if (index % 2 == 0) {
+    //         el.style.backgroundColor = '#f7f2c9';
+    //     }
+    // });    
+    
+    // btnShow.forEach(el => {
+    //     el.addEventListener('click', ()=> {
+    //         tableShow.forEach(el => {
+    //             el.classList.toggle('active')
+    //         })
+    //     })
+    // })
+
+    // btnShow.forEach((btn, index) => {
+    //     btn.addEventListener('click', () => {
+    //         const table = tableShow[index];
+    //         table.classList.toggle('active');
+    //     });
+    // });
+
+    let tables = document.querySelectorAll('.services__table');
+
+    tables.forEach(table => {
+        let tableRow = table.querySelectorAll('.services__table-item--row');  
+        let tableShow = table.querySelectorAll('.services__table-item--show');
+        let btnShow = table.querySelector('.services__table-show');
+        tableRow.forEach((el, index) => {
+            if (index % 2 == 0) {
+                el.style.backgroundColor = '#f7f2c9';
+            }
+        });
+        btnShow.addEventListener('click', () => {
+            tableShow.forEach(el => {
+                el.classList.toggle('active');
+            });
+        });
+    });
 }
